cca3491d6d5634880850d5ccd6fa5ff62c649c3f
ZumTheZazaKing <zahidi85543@gmail.com>
2024-12-16 00:03:24 +0800

    Got some stuff going:

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { Suspense, lazy } from "react";
+import { Suspense, createContext, lazy, useMemo, useReducer } from "react";
 import Footer from "./components/Footer";
 import Loader from "./components/Loader";
 
@@ -10,15 +10,52 @@ const Game = lazy(() =>
   import("./pages/Game").then((module) => ({ default: module.Game }))
 );
 
+export const Context = createContext({});
+
 function App() {
+  const initialState = {
+    start: false,
+    time: 0,
+    gameState: [
+      ["", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", ""],
+    ],
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      default:
+        return { ...state };
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const memo = useMemo(
+    () => ({
+      state,
+      dispatch,
+    }),
+    [state]
+  );
+
   return (
     <Router>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/game" element={<Game />} />
-        </Routes>
-      </Suspense>
+      <Context.Provider value={memo}>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/game" element={<Game />} />
+          </Routes>
+        </Suspense>
+      </Context.Provider>
       <Footer />
     </Router>
   );
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { Context } from "../App";
+
 export const Game = () => {
+  const { state } = useContext(Context);
+
   return (
     <main>
-      <h1>Game</h1>
+      <h1>Game {state.time}</h1>
     </main>
   );
 };
